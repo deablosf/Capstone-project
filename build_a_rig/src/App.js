c3151a4f4b62c05429645ab682f1eba2e7d84cbd
Student_PSXGA136 <deablosf@hotmail.com>
2020-09-09 13:29:20 -0400

    starting the clean up

--- build_a_rig/src/App.js
+++ build_a_rig/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Cases from './components/Cases';
 import Cpu from './components/Cpu';
@@ -6,30 +12,60 @@ import MotherBoards from './components/MotherBoards';
 import Ram from './components/Ram';
 import Storage from './components/Storage';
 import VideoCards from './components/VideoCards';
+import Build from './components/Build';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="menu">
-          <div>B-a-R</div>
-          <div>Cases</div>
-          <div>MotherBoards</div>
-          <div>CPUs</div>
-          <div>RAM</div>
-          <div>STORAGE</div>
-          <div>VID Cards</div>
-          <div>BUILD!</div>
-        </div>
-       
-      </header>
+      <Router>
+        <div>
+        <header className="App-header">
+          <div className="menu">
+            <div><Link to="">B-a-R</Link></div>
+            <div><Link to="case">Cases</Link></div>
+            <div><Link to="boards">MotherBoards</Link></div>
+            <div><Link to="cpu">CPUs</Link></div>
+            <div><Link to="ram">RAM</Link></div>
+            <div><Link to="storage">STORAGE</Link></div>
+            <div><Link to="video">VID Cards</Link></div>
+            <div><Link to="build">BUILD!</Link></div>
+          </div>
+        
+        </header>
 
-      <div className="welcome"> 
+        <div className="welcome"> 
         <div className="title">BUILD A RIG</div>
-        <div className="welTitle"><h1>WELCOME</h1></div>
-        <div className="text"><p>Welcome to build a Rig, a site to help computer enthuseists build the computers they desire. This site helps by alllowing youto see whither the parts you want are compatable with eachother.</p></div>
       </div>
+        
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL.  */}
+         <Switch>
+          <Route path="/case">
+            <Cases />
+          </Route>
+          <Route path="/boards">
+            <MotherBoards />
+          </Route>
+          <Route path="/cpu">
+            <Cpu />
+            <Route path="/ram">
+            <Ram />
+          </Route>
+          <Route path="/storage">
+            <Storage />
+          </Route>
+          <Route path="/video">
+            <VideoCards />
+            <Route path="/build">
+              <Build />
+            </Route>
+          </Route>
+          </Route>
+        </Switch> 
+        </div>
+      </Router>
+      
 
 
       {/* <Cases />
