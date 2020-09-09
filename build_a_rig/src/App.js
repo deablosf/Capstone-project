1aec8210f51ca90d0dbec3c6cde08465ac026637
Student_PSXGA136 <deablosf@hotmail.com>
2020-09-09 11:50:48 -0400

    Select kind of works

--- build_a_rig/src/App.js
+++ build_a_rig/src/App.js
@@ -13,24 +13,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="menu">
-          <div>WELCOME</div>
-          <div>START</div>
+          <div>B-a-R</div>
+          <div>Cases</div>
+          <div>MotherBoards</div>
+          <div>CPUs</div>
+          <div>RAM</div>
+          <div>STORAGE</div>
+          <div>VID Cards</div>
+          <div>BUILD!</div>
         </div>
-        <div className="title">BUILD A RIG</div>
+       
       </header>
 
-      <div className="welcome">
+      <div className="welcome"> 
+        <div className="title">BUILD A RIG</div>
         <div className="welTitle"><h1>WELCOME</h1></div>
         <div className="text"><p>Welcome to build a Rig, a site to help computer enthuseists build the computers they desire. This site helps by alllowing youto see whither the parts you want are compatable with eachother.</p></div>
       </div>
 
 
-      <Cases />
+      {/* <Cases />
       <MotherBoards />
       <Cpu />
       <Ram />
       <VideoCards />
-      <Storage />
+      <Storage /> */}
       <footer>
         <div className="credits">
           Built in a Rush by <span>Sam Carter</span>
